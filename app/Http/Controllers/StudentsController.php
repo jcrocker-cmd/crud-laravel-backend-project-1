dd2019e086133ebe755ee2a6dcf0f2fd94d214f8
John Christian B. Narbaja <56188640+jcrocker-cmd@users.noreply.github.com>
2023-06-09 23:34:40 +0800

    Update StudentsController.php

--- app/Http/Controllers/StudentsController.php
+++ app/Http/Controllers/StudentsController.php
@@ -42,7 +42,40 @@ class StudentsController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validator = Validator::make($request->all(),[
+            'name' => 'required|max:191',
+            'course' => 'required|max:191',
+            'email' => 'required|email|max:191',
+            'phone' => 'required|max:10|min:10',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 422,
+                'errors' => $validator->messages(),
+            ], 422);
+        } else {
+           $students = Students::create([
+                'name' => $request->name,
+                'course' => $request->course,
+                'email' => $request->email,
+                'phone' => $request->phone,
+           ]);
+
+           if ($students) {
+            return response()->json([
+                'status' => 200,
+                'message' => 'Student Added Succesfully',
+            ], 200);
+           } else {
+            return response()->json([
+                'status' => 500,
+                'message' => 'Something Went Wrong',
+            ], 500);
+           }
+
+        }
+        
     }
 
     /**
